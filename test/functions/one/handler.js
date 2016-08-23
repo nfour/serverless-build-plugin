f0230389d939798d8b444c20a79ceebafe3b22dc
nfour <novus.nfour@gmail.com>
2016-08-23 12:00:08 +1000

    Add single function deploy

--- test/functions/one/handler.js
+++ test/functions/one/handler.js
@@ -3,5 +3,5 @@ import number from '../../lib/one'
 export function handler(event, context, done) {
     console.log({ number })
 
-    done(null, { message: number })
+    done(null, { message: number + number })
 }
